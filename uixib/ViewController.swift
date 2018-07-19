ee6502685a43b70c119279b52cebd76f82cf999e
PM Academy 3 <pmacademy3@PMs-Mac-mini-2.local>
2018-07-19 17:05:16 +0700

    going home

---
+++ uixib/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  uixib
+//
+//  Created by PM Academy 3 on 7/19/18.
+//  Copyright © 2018 PM Academy 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+
+}
